ec3e136fef12f8d450fa6be5fe654ef2a1c77659
chatis <chatisek2420@gmail.com>
2024-05-12 23:06:34 +0200

    feat: selecting cities from list in dialog

---
+++ lib/models/country_dropdown_menu.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:jakosc_powietrza/data_models/city_id.dart';
+
+class CityDropdownMenu extends StatefulWidget {
+  const CityDropdownMenu(
+      {super.key, required this.futureCityID, required this.onCountrySelected});
+  final List<CityID> futureCityID;
+  final Function(String) onCountrySelected;
+
+  @override
+  State<CityDropdownMenu> createState() => _CityDropdownMenuState();
+}
+
+class _CityDropdownMenuState extends State<CityDropdownMenu> {
+  late String currentValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownMenu<String>(
+      initialSelection: widget.futureCityID.first.id.toString(),
+      onSelected: (String? value) {
+        setState(() {
+          currentValue = value!;
+        });
+        widget.onCountrySelected(value!);
+      },
+      dropdownMenuEntries:
+          widget.futureCityID.map<DropdownMenuEntry<String>>((CityID value) {
+        return DropdownMenuEntry<String>(
+            value: value.id.toString(), label: value.stationName);
+      }).toList(),
+    );
+  }
+}
--- lib/models/main_data.dart
+++ lib/models/main_data.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jakosc_powietrza/data_models/city_id.dart';
+import 'package:jakosc_powietrza/models/choose_station_dialog.dart';
 import 'package:jakosc_powietrza/models/choosen_city.dart';
 import 'package:jakosc_powietrza/models/city_data.dart';
 import 'package:http/http.dart' as http;
@@ -16,22 +17,32 @@ class MainData extends StatefulWidget {
 
 class _MainDataState extends State<MainData> {
   late List<CityID> futureCityID;
+  List<String> selectedCities = [];
 
   @override
   void initState() {
     super.initState();
     _fetchData();
+    //TODO Load selectedCities from memory if wanted
   }
 
-  _fetchData() async {
-      final response = await http
-      .get(Uri.parse('https://api.gios.gov.pl/pjp-api/rest/station/findAll'));
+  void _fetchData() async {
+    final response = await http
+        .get(Uri.parse('https://api.gios.gov.pl/pjp-api/rest/station/findAll'));
     if (response.statusCode == 200) {
       final decodedJson = jsonDecode(response.body);
-      futureCityID = decodedJson.map<CityID>((item) => CityID.fromJson(item)).toList() ; 
+      futureCityID =
+          decodedJson.map<CityID>((item) => CityID.fromJson(item)).toList();
+      futureCityID.sort((a, b) => a.stationName.compareTo(b.stationName));
     } else {
       throw Exception('Failed to load album');
     }
+  }
+
+  void _onDialogSaved(List<String> cities) {
+    setState(() {
+      selectedCities = cities;
+    });
   }
 
   @override
@@ -58,20 +69,19 @@ class _MainDataState extends State<MainData> {
           child: ListView(
             scrollDirection: Axis.horizontal,
             padding: const EdgeInsets.all(8),
-            children: const <Widget>[
-              SizedBox(
+            children: <Widget>[
+              const SizedBox(
                 width: 10,
               ),
-              CityData(
+              const CityData(
                 cityName: 'malaga',
               ),
-              CityData(cityName: 'tikitaki'),
-              CityData(
-                cityName: 'i',
-              ),
-              CityData(
-                cityName: 'kasztanki',
-              ),
+              ...selectedCities.map((cityId) => CityData(
+                    cityName: futureCityID
+                        .firstWhere(
+                            (element) => element.id.toString() == cityId)
+                        .stationName,
+                  )),
             ],
           ),
         ),
@@ -84,7 +94,12 @@ class _MainDataState extends State<MainData> {
             style: TextStyle(
                 color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
           ),
-          onPressed: () {},
+          onPressed: () => showDialog<List<String>>(
+            context: context,
+            builder: (BuildContext context) => Dialog.fullscreen(
+                child: ChooseStationDialog(
+                    futureCityID: futureCityID, onDialogSaved: _onDialogSaved)),
+          ),
         ),
         const SizedBox(
           height: 10,
